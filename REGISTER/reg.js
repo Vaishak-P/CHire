8f47c9b761ba400078d49fa250bf45161e8a34b3
Vaishak-P <vaishakpanakkat@gmail.com>
2024-02-17 17:57:57 +0530

    second commit

---
+++ REGISTER/reg.js
@@ -0,0 +1,13 @@
+// const button=document.getElementById("button1");
+// const rightbox=document.querySelector(".right-box");
+// button1.addEventListener("click",()=>{
+//     rightbox.classList.toggle("visible");
+// });
+
+// function toggleSelected(){
+//     if (rightBox.style.display === "none") {
+//         rightBox.style.display = "block";
+//       } else {
+//         rightBox.style.display = "none";
+//       }
+// };
--- test/a.js
+++ test/a.js
@@ -1,11 +1,11 @@
-const loginBtn = document.getElementById("loginBtn");
-const loginPopup = document.getElementById("loginPopup");
-const closeButton = document.querySelector(".close-button");
+// const loginBtn = document.getElementById("loginBtn");
+// const loginPopup = document.getElementById("loginPopup");
+// const closeButton = document.querySelector(".close-button");
 
-loginBtn.addEventListener("click", () => {
-  loginPopup.classList.remove("hidden");
-});
+// loginBtn.addEventListener("click", () => {
+//   loginPopup.classList.remove("hidden");
+// });
 
-closeButton.addEventListener("click", () => {
-  loginPopup.classList.add("hidden");
-});
+// closeButton.addEventListener("click", () => {
+//   loginPopup.classList.add("hidden");
+// });
