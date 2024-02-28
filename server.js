9b11df9d035d314af4a5dbf0334f6a6612da622a
akshaykumarsonu <ak6111148@gmail.com>
2024-02-28 08:35:06 +0530

    Login module integrated with database

---
+++ server.js
@@ -0,0 +1,68 @@
+const mysql = require('mysql');
+const express = require('express');
+const bodyparser = require('body-parser');
+const path = require('path');
+
+var app = express();
+
+// Set view engine and configure body-parser
+app.set('view engine', 'ejs');
+app.use(bodyparser.urlencoded({ extended: true }));
+
+// Set up static files middleware
+app.use(express.static(path.join(__dirname, 'CHireMain')));
+
+// MySQL Connection Configuration
+const mysqlConnection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'Akshay@123',
+    database: 'cleverhire'
+});
+
+mysqlConnection.connect((err) => {
+    if (!err)
+        console.log('DB connection succeeded.');
+    else
+        console.log('DB connection failed \n Error: ' + JSON.stringify(err, undefined, 2));
+});
+
+app.get('/',(req,res)=>{
+    res.render('D:/mainProject/CHire/CHireMain/index')
+})
+
+// Login authentication route
+app.post('/LOGIN/login.html', (req, res) => {
+    const email = req.body.email;
+    const password = req.body.psw;
+    mysqlConnection.query('SELECT * FROM student WHERE email = ?', email, (err, results) => {
+        if (err) {
+            console.error('Error executing query: ', err);
+            res.status(500).send('Internal Server Error');
+            return;
+        }
+
+        if (results.length === 0) {
+            // User not found
+            //res.status(401).render('D:/backend/CHire-main/index', { error: 'Invalid credentials. Please try again.' });
+            res.status(401).send('Invalid username or password');
+            return;
+        }
+
+        const user = results[0];
+        if (user.password !== password) {
+            // Incorrect password
+            res.status(401).send('Invalid username or password');
+            return;
+        }
+
+        // Authentication successful
+        // Render the EJS file with the user's details
+        res.render('D:/mainProject/CHire/CHireMain/std-dashboard/std-dashboard', {userName:user.name,cgpa:user.cgpa,sem:user.sem,test:user.mocktest_score,fluency:user.fluency_score,internships:user.internships,phn:user.phone,mail:user.email,address:user.address,bld:user.blood_group,total:user.total });
+    });
+});
+
+// Start the server
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
